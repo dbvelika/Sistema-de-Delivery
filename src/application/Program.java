c8c780b4c0974e7e10aa448231e6b281b2d6af3d
Danilo Velika <danilobvelika@gmail.com>
2026-04-11 13:02:11 -0300

    Criando o menu base no programa principal e criacao do pacote views para submenus

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,5 +9,34 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        int opcao = 0;
+        do{
+            System.out.println("========== SISTEMA DE DELIVERY ==========");
+            System.out.println("1 - Gerenciar Restaurantes");
+            System.out.println("2 - Gerenciar Produtos");
+            System.out.println("3 - Gerenciar Clientes");
+            System.out.println("4 - Gerenciar Entregadores");
+            System.out.println("5 - Gerenciar Pedidos");
+            System.out.println("6 - Gerar Relatório");
+            System.out.println("0 - Sair");
+            System.out.println("=========================================");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = sc. nextInt();
+
+           switch (opcao){
+               case 1: System.out.println("====== MENU DE RRESTAURANTESES ======"); break;
+               case 2: System.out.println("====== MENU DE PRODUTOS ======"); break;
+               case 3: System.out.println("====== MENU DE CLIENTES ======"); break;
+               case 4: System.out.println("====== MENU DE ENTREGADORES ======"); break;
+               case 5: System.out.println("====== MENU DE PEDIDOS ======"); break;
+               case 6: System.out.println("====== RELATÓRIO ======"); break;
+               case 0: System.out.println("Encerrando..."); break;
+               default: System.out.println("Opção inválida!");
+           }
+        }while(opcao != 0);
+
+        sc.close();
     }
 }
